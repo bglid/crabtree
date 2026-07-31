f06225d7e4f4e0910782b26b5cfd63888850a1dd
bglid <benjamin.h.glidden@gmail.com>
2026-07-31 07:40:36 -0400

    Adding resolve dir + tests

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use std::{path::PathBuf, process::ExitCode};
 use clap::Parser;
 use cli::Cli;
 
+fn resolve_directory(path_buf: Option<PathBuf>) -> Result<PathBuf> {
+    match path_buf {
+        Some(dir) => Ok(dir),
+        None => std::env::current_dir().context("Failed to get current directory"),
+    }
+}
+
 fn main() -> ExitCode {
     let args = Cli::parse();
 
@@ -16,12 +23,31 @@ fn main() -> ExitCode {
 }
 
 fn run(args: Cli) -> Result<ExitCode> {
-    let dirs: PathBuf = match args.directory {
-        Some(dir) => dir,
-        None => std::env::current_dir().context("Failed to get current directory")?,
-    };
+    let dirs = resolve_directory(args.directory)?;
 
     println!("Current dir:\n {}", dirs.display());
 
     Ok(ExitCode::SUCCESS)
+}
+
+// TESTS! :-[)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn uses_dir_from_arg() {
+        let expected_dir = PathBuf::from("/test/hello");
+
+        let actual = resolve_directory(Some(expected_dir.clone()))
+            .unwrap_or_else(|_err| panic!("Failure in resolving path"));
+        assert_eq!(actual, expected_dir)
+    }
+
+    #[test]
+    fn uses_cd_when_no_arg() {
+        let expected_dir = std::env::current_dir().expect("Error in getting cd");
+        let actual = resolve_directory(None).expect("Error in resolving directory");
+        assert_eq!(actual, expected_dir)
+    }
 }
