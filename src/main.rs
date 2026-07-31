7a6829727d60557ded1a108fdd82fa34e91f19a3
bglid <benjamin.h.glidden@gmail.com>
2026-07-31 06:48:41 -0400

    adding run and anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 mod cli;
-use std::path::PathBuf;
+use anyhow::{Context, Result};
+use std::{path::PathBuf, process::ExitCode};
 
 use clap::Parser;
 use cli::Cli;
 
-fn main() {
+fn main() -> ExitCode {
     let args = Cli::parse();
 
     println!("Searching...");
+    run(args).unwrap_or_else(|err| {
+        eprintln!("Error, {}", err);
+        ExitCode::FAILURE
+    })
+}
+
+fn run(args: Cli) -> Result<ExitCode> {
     let dir = args.directory.unwrap_or_else(|| {
         std::env::current_dir().unwrap_or_else(|err| {
             eprintln!("Error getting current directory {}", err);
             PathBuf::from(".")
         })
     });
-
     println!("Current dir:\n {}", dir.display());
+
+    Ok(ExitCode::SUCCESS)
 }
