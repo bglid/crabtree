9bb55099c92da64c7c5b97e3374ac0a2947f7ef9
bglid <benjamin.h.glidden@gmail.com>
2026-08-25 07:28:14 -0400

    little refactor for cleanliness

--- src/viz.rs
+++ src/viz.rs
@@ -1,6 +1,6 @@
 use crate::{
-    tree::{EntryType, Tree, TreeEntry, TreeIter},
-    viz::TreePart::{FinalFile, TreeBranch, TreeFile},
+    tree::{Tree, TreeEntry},
+    viz::TreePart::{FinalEnt, NormalEnt},
 };
 
 // better visualizing chars
@@ -9,47 +9,28 @@ const LEAF: char = '└';
 const HOR: char = '─';
 const VERT: char = '│';
 
-// enum for which point in the tree it is?
+// enum for which point in the tree it is
 enum TreePart {
-    TreeBranch,
-    TreeFile,
-    FinalFile,
+    NormalEnt,
+    FinalEnt,
 }
 
 impl TreePart {
     fn resolve_part(entry: &TreeEntry) -> Self {
         if entry.last_entry {
-            return FinalFile;
+            return FinalEnt;
         }
-
-        // placeholder
-        match entry.entrytype {
-            EntryType::Dir => TreeBranch,
-            EntryType::File => TreeFile,
-            EntryType::SymL => TreeFile,
-            EntryType::Other => unimplemented!(),
-        }
+        NormalEnt
     }
 
     fn print_ancestors(&self, e: &TreeEntry) {
-        // for anc in 0..e.ancestor_has_sib.len() {
-        //     if anc == 0 {
-        //         continue;
-        //     }
-        //     if e.ancestor_has_sib[anc] {
-        //         print!("{}{}", VERT, " ".repeat(e.depth.saturating_sub(anc)),);
-        //     } else {
-        //         print!("{}", " ".repeat(e.depth.saturating_sub(anc)),);
-        //     }
-        // }
         let res: String = e
             .ancestor_has_sib
             .iter()
             .skip(1)
             .map(|has_sib| {
-                // let indent = String::from(" ");
                 if *has_sib {
-                    format!("{}{}", VERT, " ")
+                    format!("{VERT} ")
                 } else {
                     "  ".to_string()
                 }
@@ -59,56 +40,34 @@ impl TreePart {
         print!("{}", res)
     }
 
+    fn print_tree_line(&self, e: &TreeEntry, tree_char: char) {
+        println!("{}{}{}", tree_char, HOR, e.path.display())
+    }
+
     fn print_tree_part(&self, e: &TreeEntry) {
         match self {
-            TreeBranch => {
+            NormalEnt => {
                 if e.depth > 0 {
                     self.print_ancestors(e);
-                    println!(
-                        "{}{}{}{}/",
-                        " ".repeat(e.depth.saturating_sub(1)),
-                        BRANCH,
-                        HOR,
-                        e.path.display()
-                    )
+                    self.print_tree_line(e, BRANCH);
                 } else {
                     println!("{}", e.path.display())
                 }
             }
-            TreeFile => {
+            FinalEnt => {
                 self.print_ancestors(e);
-                println!(
-                    "{}{}{}{}",
-                    " ".repeat(e.depth.saturating_sub(1)),
-                    BRANCH,
-                    HOR,
-                    e.path.display()
-                )
-            }
-            FinalFile => {
-                self.print_ancestors(e);
-                println!(
-                    "{}{}{}{}",
-                    " ".repeat(e.depth.saturating_sub(1)),
-                    LEAF,
-                    HOR,
-                    e.path.display()
-                )
+                self.print_tree_line(e, LEAF);
             }
         }
     }
 }
 
 pub fn visualize_tree(tree: Tree) {
-    let mut t_iter: TreeIter = tree.into_iter();
-    while let Some(t_ent) = t_iter.next() {
-        match t_ent {
-            Ok(e) => {
-                let part = TreePart::resolve_part(&e);
-                // part.print_ancestors(&e);
-                part.print_tree_part(&e);
-            }
-            Err(e) => eprintln!("{}", e),
+    tree.into_iter().for_each(|t_entry| match t_entry {
+        Ok(ent) => {
+            let part = TreePart::resolve_part(&ent);
+            part.print_tree_part(&ent);
         }
-    }
+        Err(e) => eprintln!("{}", e),
+    });
 }
